e05fce4b626333ab84e7a42bf015b64d25e80217
cvcvcx9 <156741717+cvcvcx9@users.noreply.github.com>
2024-06-15 12:37:28 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240615/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        int temp,temp2;
+        temp = b;
+        b = a;
+        temp2 = c;
+        c = temp;
+        a = temp2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
